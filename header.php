8a9c84da71b09f80b3f1e66cd1b520bd8072a402
MohGumaa <gomaam14@gmail.com>
2022-07-24 13:44:12 +0400

    update the style

--- header.php
+++ header.php
@@ -14,11 +14,35 @@ $bootstrap_version = get_theme_mod( 'understrap_bootstrap_version', 'bootstrap4'
 $navbar_type       = get_theme_mod( 'understrap_navbar_type', 'collapse' );
 ?>
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html dir="rtl" lang="ar">
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<title><?php if (is_front_page()) :?> <?php bloginfo('name'); ?> - <?php bloginfo('description'); ?><?php else: ?><?php wp_title(); ?><?php endif; ?></title>
+	
+	<style>
+		@font-face {
+			font-family: 'Helvetica-Neue-SKY-Bd';
+			src: url(<?php echo get_theme_file_uri( 'fonts/HelveticaBd/Helvetica\ Neue\ W23\ for\ SKY\ Bd.otf' ); ?>);
+			src: url(<?php echo get_theme_file_uri( 'fonts/HelveticaBd/Helvetica\ Neue\ W23\ for\ SKY\ Bd.ttf' ); ?>);
+			src: url(<?php echo get_theme_file_uri( 'fonts/HelveticaBd/Helvetica\ Neue\ W23\ for\ SKY\ Bd.woff' ); ?>);
+			font-weight: bold;
+			font-style: normal;
+			font-display: swap;
+		}
+
+		@font-face {
+			font-family: 'Helvetica-Neue-SKY-Reg';
+			src: url(<?php echo get_theme_file_uri( 'fonts/HelveticaReg/Helvetica\ Neue\ W23\ for\ SKY\ Reg.otf' ); ?>);
+			src: url(<?php echo get_theme_file_uri( 'fonts/HelveticaReg/Helvetica\ Neue\ W23\ for\ SKY\ Reg.ttf' ); ?>);
+			src: url(<?php echo get_theme_file_uri( 'fonts/HelveticaReg/Helvetica\ Neue\ W23\ for\ SKY\ Reg.woff' ); ?>);
+			font-weight: 400;
+			font-style: normal;
+			font-display: swap;
+		}
+	</style>
+
 	<?php wp_head(); ?>
 </head>
 
